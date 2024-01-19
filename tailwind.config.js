e64545f4f3ddd9ab00e53bf64a896691a357c69f
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-01-20 00:53:57 +0800

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html, js}"
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['Helvetica', 'Arial', 'sans-serif'],
+      'roboto': ['Roboto']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
